7adf28f51041ab352036177e612133136500cf40
khris <khris56@hotmail.com>
2019-11-28 21:01:49 -0500

    suscripciones added room field

--- app/controllers/suscriptions_controller.rb
+++ app/controllers/suscriptions_controller.rb
@@ -1,2 +1,43 @@
 class SuscriptionsController < ApplicationController
+  def index
+    @suscriptions = Suscription.all
+  end
+
+  def new
+    @suscription = Suscription.new
+  end
+
+  def create
+    @suscription = Suscription.new(suscription_params)
+    if @suscription.save
+      redirect_to suscriptions_path, notice: "La Suscripcion fue creada"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @suscription = Suscription.find(params[:id])
+  end
+
+  def update
+    @suscription = Suscription.find(params[:id])
+    if @suscription.update(suscription_params)
+      redirect_to suscriptions_path, notice: "La Suscripción fue actualizada"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    suscription = Suscription.find(params[:id])
+    suscription.destroy
+
+    redirect_to suscriptions_path, notice: "La Suscriprión fue eliminada"
+  end
+
+  private
+   def suscription_params
+     params.require(:suscription).permit(:name, :status, :user_id, :hours, :rooms, :observations)
+   end
 end
