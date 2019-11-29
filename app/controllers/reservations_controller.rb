f6cc4aebe55c979889455c03a391048122f837ab
khris <khris56@hotmail.com>
2019-11-28 21:41:54 -0500

    reservations form implemented

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,24 @@
 class ReservationsController < ApplicationController
+  def index
+    @reservations = Reservation.all
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to reservations_path, notice: "La reserva fue creada"
+    else
+      render :new
+    end
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:suscription_id, :schedule_id)
+  end
+
 end
