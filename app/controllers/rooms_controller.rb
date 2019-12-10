dbf10ef3819fa67bd59855961f69308e6ee6d681
khris <khris56@hotmail.com>
2019-12-10 15:43:58 -0500

    rooms implemented available

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,18 @@ class RoomsController < ApplicationController
   before_action :authenticate_student!
 
   def index
-    @rooms = Room.all
+
+    @user_conected = User.find_by(email: current_student.email)
+    @active_suscription = Suscription.find_by(user_id: @user_conected.id, status: true)
+    @array_of_rooms = @active_suscription.rooms.split(',')
+    new_array = []
+    @array_of_rooms.each do |r|
+      room = Room.find_by(id: r)
+      new_array.push(room)
+    end
+
+    @rooms = new_array
+
+
   end
 end
