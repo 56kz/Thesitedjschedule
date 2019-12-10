b1148e8090f48964984305158130aa352951bc97
khris <khris56@hotmail.com>
2019-12-10 13:15:08 -0500

    devise Student implemnted

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :students
   root 'rooms#index'
   resources :reservations
   # resources :schedules
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_194710) do
+ActiveRecord::Schema.define(version: 2019_12_10_181048) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer "schedule_id"
@@ -34,6 +34,18 @@ ActiveRecord::Schema.define(version: 2019_11_28_194710) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_schedules_on_room_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_students_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end
 
   create_table "suscriptions", force: :cascade do |t|
