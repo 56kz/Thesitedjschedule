d1f96efd44fb54b14be98803fafb4be03e60021f
Andrés Soto <andres.soto.h@outlook.com>
2019-12-23 22:49:13 -0500

    load seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,14 +16,14 @@ if Suscription.count < 2
   Suscription.create(name: 6, status: true, hours: 3, observations: "Suscription de prueba para School", rooms: "1,2,4", user_id: 2)
 end
 
-# if Schedule.count < 1
-#   (Date.today.. Date.today+1.week).each do |d|
-#     block = [6, 8, 10, 12, 14, 16, 18, 20]
-#     block.each do |b|
-#       Schedule.create(day: d, hour: b, room_id: 1)
-#     end
-#   end
-# end
+if Schedule.count < 1
+   (Date.today.. Date.today+1.week).each do |d|
+     block = [6, 8, 10, 12, 14, 16, 18, 20]
+     block.each do |b|
+       Schedule.create(day: d, hour: b, room_id: 1)
+     end
+   end
+end
 
 if Reservation.count < 1
   Reservation.create(schedule_id: 1, suscription_id: 1, start_hour: 8, reserve_date: '2019-12-10', room: 1)
