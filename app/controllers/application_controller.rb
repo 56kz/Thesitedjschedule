184026e5cfcb67982cb786b109a6eaa983508f68
Andrés Soto <andres.soto.h@outlook.com>
2021-10-29 21:00:13 -0500

    fix change password error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   respond_to :html, :json
+
+  before_action :set_variable, :authenticate_student!
+
+  def set_variable
+      @user_conected = User.find_by(email: current_student.email)
+  end
   
 end
