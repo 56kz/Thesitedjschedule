f0e2a3d35a1fdc5ec967a5f5b40e66bda4b62172
Andres Soto <andres.soto.h@outlook.com>
2025-02-23 21:17:00 -0500

    Fix error on enum values

--- app/models/suscription.rb
+++ app/models/suscription.rb
@@ -3,5 +3,6 @@ class Suscription < ApplicationRecord
   has_many :reservations, dependent: :destroy
 
   enum :name, [:MV1, :MV1N2, :MV2, :MV2N2, :MV3, :MV3N2, :P1, :P2, :P3, :P4, :P5, :Bloque, :Demo, :Otro]
-  enum :hours, [:Dos, :Cuatro, :Diez, :Doce, :Full]
+  enum :hours, [:Dos, :Cuatro, :Doce, :Full, :Diez]
+
 end
