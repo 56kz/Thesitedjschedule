1114c852935e9ebdb892c0abc3d321f5fdd4e9d8
Andres Soto <andres.soto.h@outlook.com>
2025-02-24 07:57:58 -0500

    Fix dates format

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,10 +44,7 @@ document.addEventListener("turbolinks:load", function () {
                             timeZone: 'UTC', 
                             year: 'numeric', 
                             month: '2-digit', 
-                            day: '2-digit', 
-                            hour: '2-digit', 
-                            minute: '2-digit', 
-                            second: '2-digit', 
+                            day: '2-digit',
                             hour12: true
                         }).format(new Date(data));
                         
